6799fb7cf9408fdb90bb3fb967c2778c3e9da5c9
이형철 <lucid80@gmail.com>
2023-10-05 10:35:24 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+setuptools.setup(
+    name="table1",
+    version="1.0.0",
+    author="Hyung-Chul Lee",
+    author_email="vital@snu.ac.kr",
+    description="Python Libray for Table 1",
+    long_description="Python Libray for Table 1",
+    long_description_content_type="text/markdown",
+    url="https://github.com/vitaldb/table1",
+    install_requires=['numpy', 'pandas', 'requests', 'wfdb'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
